cbea6031c8a205eb503e10ae6890c73bc2587fd4
XiNGRZ <chenxingyu92@gmail.com>
2020-11-14 02:35:28 +0800

    btn: Make long press great again

--- main/task_btn.c
+++ main/task_btn.c
@@ -3,6 +3,7 @@
 static const char *TAG = "task_btn";
 
 #define PIN_KEY_USER GPIO_NUM_0
+#define LONG_PRESS_TIMEOUT_MS 1000
 
 static xQueueHandle btn_q = NULL;
 static TickType_t btn_down_ticks = 0;
@@ -30,6 +31,34 @@ drv_gpio_init()
 	gpio_isr_handler_add(PIN_KEY_USER, drv_gpio_event, (void *)PIN_KEY_USER);
 }
 
+static void
+btn_on_pressed(void)
+{
+	ESP_LOGI(TAG, "Pressed");
+}
+
+static void
+btn_on_long_pressed(void)
+{
+	ESP_LOGI(TAG, "Long pressed");
+	wlan_reset();
+	lcd_show_qrcode();
+	gc9a01_set_backlight(GC9A01_BACKLIGHT_MAX);
+	blec_adv_start();
+}
+
+static void
+btn_on_down(void)
+{
+	ESP_LOGV(TAG, "Down");
+}
+
+static void
+btn_on_up(void)
+{
+	ESP_LOGV(TAG, "Up");
+}
+
 void
 btn_proc_task(void *arg)
 {
@@ -42,26 +71,34 @@ btn_proc_task(void *arg)
 	uint32_t num;
 	int val;
 	while (1) {
+		if (btn_down_ticks > 0) {
+			val = gpio_get_level(num);
+			if (val) {
+				btn_down_ticks = 0;
+				btn_on_pressed();
+			} else {
+				TickType_t ticks = xTaskGetTickCount();
+				uint32_t hold_ms = (ticks - btn_down_ticks) * portTICK_PERIOD_MS;
+				if (hold_ms >= LONG_PRESS_TIMEOUT_MS) {
+					btn_down_ticks = 0;
+					btn_on_long_pressed();
+				} else {
+					goto next;
+				}
+			}
+		}
+
 		if (xQueueReceive(btn_q, &num, portMAX_DELAY) != pdPASS) {
 			goto next;
 		}
 
 		val = gpio_get_level(num);
-		ESP_LOGI(TAG, "GPIO: %d=%d", num, val);
-
 		if (!val) {
 			btn_down_ticks = xTaskGetTickCount();
+			btn_on_down();
 		} else {
-			TickType_t ticks = xTaskGetTickCount();
-			uint32_t hold_ms = (ticks - btn_down_ticks) * portTICK_PERIOD_MS;
-			ESP_LOGI(TAG, "Button hold: %d ms", hold_ms);
-
-			if (hold_ms > 1000) {
-				wlan_reset();
-				lcd_show_qrcode();
-				gc9a01_set_backlight(GC9A01_BACKLIGHT_MAX);
-				blec_adv_start();
-			}
+			btn_down_ticks = 0;
+			btn_on_up();
 		}
 
 	next:
